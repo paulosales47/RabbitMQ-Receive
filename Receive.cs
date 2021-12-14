9288b4049d27e64860e1b0103081e89df3c2446c
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-12-14 00:53:09 -0300

    Recebendo mensagens para filas de trabalho

--- Receive.cs
+++ Receive.cs
@@ -12,17 +12,22 @@ public class Receive{
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
-            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
-
+            channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            channel.BasicQos(0, 1, false);
+            
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) => 
             { 
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 System.Console.WriteLine("[x] Received {0}", message);
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 3000);
+                Console.WriteLine("[X] Done");
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
             };
 
-            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+            channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
 
             Console.WriteLine("Press [enter] to exit");
             Console.ReadKey();
