34eb42b2dc49613f6741c9c47748331efa3bc4b5
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-12-15 23:29:55 -0300

    RPC

--- Receive.cs
+++ Receive.cs
@@ -6,35 +6,57 @@ using System.Text;
 
 public class Receive{
     
-    public static void Main(string[] args){
-
-        if(args.Length < 1)
-            return;
+    public static void Main(){
 
         var factory = new ConnectionFactory(){ HostName = "localhost" };
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
-            channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
-            var queueName = channel.QueueDeclare().QueueName;
-
-            System.Console.WriteLine("[*] Waiting for logs");
-
-            foreach(var topic in args){
-                channel.QueueBind(queue: queueName, exchange: "topic_logs", routingKey: topic);
-            }
+            channel.QueueDeclare(queue: "rpc_queue", durable : false, exclusive: false, autoDelete: false, arguments: null);
+            channel.BasicQos(0, 1, false);
 
             var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (model, ea) => 
-            { 
-                var body = ea.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                var routingKey = ea.RoutingKey;
+            channel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
+            System.Console.WriteLine("[x] Awaiting RPC requests");
 
-                System.Console.WriteLine("[x] Received '{0}':'{1}'", routingKey, message);
+            consumer.Received += (model, ea) => 
+            {
+                string? response = default;
+                
+                var body = ea.Body.ToArray();
+                var props = ea.BasicProperties;
+                var replayProps = channel.CreateBasicProperties();
+                replayProps.CorrelationId = props.CorrelationId;
+
+                try
+                {
+                    var message = Encoding.UTF8.GetString(body);
+                    int n = int.Parse(message);
+
+                    Console.WriteLine("[.] fib({0})", message);
+                    response = fib(n).ToString();
+                }
+                catch(Exception ex){
+                    Console.WriteLine("[.]" + ex.Message);
+                    response = "";
+                }
+                finally
+                {
+                    var responseBytes = Encoding.UTF8.GetBytes(response!);
+                    channel.BasicPublish
+                    (
+                        exchange: "",
+                        routingKey: props.ReplyTo,
+                        basicProperties: replayProps,
+                        body: responseBytes
+                    );
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                }
+
             };
 
-            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+            
 
             Console.WriteLine("Press [enter] to exit");
             Console.ReadKey();
@@ -42,4 +64,10 @@ public class Receive{
 
     }
 
+    private static int fib(int n){
+        if(n == 0 || n == 1)
+            return n;
+        
+        return fib(n - 1) + fib(n - 2);
+    }
 }
